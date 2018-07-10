f10cfcda1370980d95856775d0bfae00a5271c42
Shane Lester <Shanejlester@gmail.com>
2018-07-09 23:25:11 -0400

    Add clickable cards which display there class name

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,25 @@
-alert("Hello world");
+// //Object to keep track of card game
+// const cardGame = {
+// 	currentTurn: Array,
+
+// 	addCard: function(newCard) {
+// 		return;
+// 	}, 
+
+// 	turnOver: function() {
+// 		return;
+// 	}
+// };
+
+function updateGame(event) {
+	alert(event.target.className);
+}
+
+//Add event listeners to each card
+const listOfCards = document.querySelectorAll(".card");
+for (let i = 0; i < listOfCards.length; i++) {
+	listOfCards.item(i).addEventListener('click', updateGame);
+}
+
+
+
