72afd5a346484785774d472140faa59ce7edab5f
Shane Lester <Shanejlester@gmail.com>
2018-07-09 23:43:00 -0400

    Make div element clickable instead of span element. This makes it so that when we click a card, the className property outputs of the div element. Recall the div element is the value of the card so we can see if it matches another card

--- javascript/script.js
+++ javascript/script.js
@@ -12,7 +12,14 @@ const cardGame = {
 };
 
 function updateGame(event) {
-	alert(event.target.className);
+	//Click directly on <div> element here
+	if (event.target.nodeName === 'DIV') {   
+    	alert(event.target.className);
+    }
+    //Clicked one of the spans, so we need to get the <div> element to check what card it is
+    else if (event.target.parentElement.nodeName === 'DIV'){
+     	alert(event.target.parentElement.className);
+    }
 }
 
 //Add event listeners to each card
