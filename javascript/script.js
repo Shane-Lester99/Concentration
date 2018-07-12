b590f106cdfa1b37a470787ad474d5167bcac9f1
Shane Lester <Shanejlester@gmail.com>
2018-07-12 01:43:14 -0400

    Add winning logic and game reset after win

--- javascript/script.js
+++ javascript/script.js
@@ -16,45 +16,74 @@ const cardGame = {
 		}
 		//If there are two cards in array, it is time to check if they are equal
 		if (this.currentTurn.length == 2) {
-			this.checkIfMatch();
+			//Check if the two cards match
+			const matchStatus = this.checkIfMatch();
+			if (matchStatus) {
+				//If they match, check if they win
+				if (this.didWin()) {
+					setTimeout(function() {
+					//Display win alert
+					alert(`You won in ${cardGame.numberOfTurns} turns!!`);			
+					//Start game over after alert is displayed
+					cardGame.startOver();
+					}, 500);
+					return;
+				}
+			}
+			//If they didnt win, reset the current turn and continue game play
 			this.currentTurn = [];
-			return;
 		}
 		return;
 	}, 
 
 	checkIfMatch: function() {	
+		let didTheyMatch = false;
 		//choose the first and second string in the array, and
 		//the string indexed at 1 is the value of the card (which)
 		//tells us wether it matches or not
 		const card1 = this.currentTurn[0][1];
 		const card2 = this.currentTurn[1][1];
 		if (card1 == card2) {
-			alert(`match of ${this.currentTurn[0]} and ${this.currentTurn[1]}`);
 			//Add cards to discard pile
 			this.discardPile.push(this.currentTurn[0]);
 			this.discardPile.push(this.currentTurn[1]);
 			//Turn color of unusable cards to red
 			document.querySelector(`.${this.currentTurn[0]}`).style.color = "red";
 			document.querySelector(`.${this.currentTurn[1]}`).style.color = "red";
+			alert(`match of ${this.currentTurn[0]} and ${this.currentTurn[1]}`);
+			didTheyMatch = true;
 		}
 		else {
 			alert(`${this.currentTurn[0]} and ${this.currentTurn[1]} dont match`);
 		}
-		this.incrementTurns();
-		return;
+		this.incrementTurns();		
+		return didTheyMatch;
 	},
 
 	incrementTurns: function() {
 		this.numberOfTurns += 1;
-		document.querySelector("#number-of-turns").textContent = this.numberOfTurns;
+		document.querySelector(".number-of-turns").textContent = this.numberOfTurns;
+		return;
 	},
 
 	 startOver: function() {
 		this.currentTurn = [];
 		this.discardPile = [];
 		this.numberOfTurns = 0;
-		document.querySelector("#number-of-turns").textContent = 0;
+		document.querySelector(".number-of-turns").textContent = 0;
+		let deckOfCards = document.querySelector("#all-cards");
+		for (let i = 0; i < deckOfCards.children.length; i++) {
+			deckOfCards.children.item(i).style.color = "black";
+		}
+		return;
+	},
+
+	didWin: function() {
+		let didIWin = false;
+		if (this.discardPile.length == 16) {
+			didIWin = true;
+		}
+		return didIWin;
 	}
 };
 
@@ -69,14 +98,12 @@ function updateGame(event) {
      	card = event.target.parentElement.classList.item(1);
     }
     cardGame.chooseCard(card);
+    return;
 }
 
 function resetGame() {
 	cardGame.startOver();
-	let deckOfCards = document.querySelector("#all-cards");
-	for (let i = 0; i < deckOfCards.children.length; i++) {
-		deckOfCards.children.item(i).style.color = "black";
-	}
+	return;
 }
 
 //Add event listeners to each card
